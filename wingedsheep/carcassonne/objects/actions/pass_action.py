21c2698d168beba86ec7bafe7e5c12d267d077a3
KeithMaxwellZ <37383813+KeithMaxwellZ@users.noreply.github.com>
2025-11-12 21:37:29 -0500

    Remove redundant pass statement in PassAction class

--- wingedsheep/carcassonne/objects/actions/pass_action.py
+++ wingedsheep/carcassonne/objects/actions/pass_action.py
@@ -4,7 +4,6 @@ from wingedsheep.carcassonne.objects.actions.action import Action
 class PassAction(Action):
     def __str__(self):
         return "pass"
-    pass
 
     def __repr__(self):
         return "PassAction()"
